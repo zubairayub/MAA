421445715794a5d6f751c05c36590f53fb8e3899
zubairayub <zubairayub@hotmail.com>
2021-08-26 16:35:04 +0500

    sql array function fixes

--- app/controller/auth/login.php
+++ app/controller/auth/login.php
@@ -1,6 +1,12 @@
 <?php
 $conn = OpenCon();
-$rows  = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM `users_info` "));
+$rows  = mysqli_query($conn,"SELECT * FROM `users_info` ");
+
+while($data = mysqli_fetch_assoc($rows)){
+
+$return[] = $data;
+
+}
 
 CloseCon($conn);
 
